a28b7269a49f2170dd4480823040d005daa18890
catalincsibi <csibicatalin@gmail.com>
2024-02-15 15:54:40 +0200

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(findMinimalPath("/foo/bar/../loo/../.."));
+        System.out.println(findMinimalPath("/foo/../.."));
+        System.out.println(findMinimalPath("/foo/./bar///.."));
+        System.out.println(findMinimalPath("/foo///.//../bar/"));
+
+    }
+
+    private static String findMinimalPath(String s) {
+        String[] strings = s.split("/");
+
+        List<String> stringList = new ArrayList<>();
+
+        for(String str : strings) {
+            if(str.isEmpty() || str.equals("."))
+                continue;
+
+            if(!str.equals("..")) {
+                stringList.add(str);
+            } else if(!stringList.isEmpty()) {
+                stringList.remove(stringList.size() -1);
+            }
+        }
+
+        if(stringList.isEmpty())
+            return "/";
+        else if (stringList.size() == 1)
+            return "/" + stringList.get(0);
+
+        return String.join("/", stringList);
+     }
+
+
+}
